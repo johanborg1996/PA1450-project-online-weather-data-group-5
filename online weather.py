d9590870ed53a5a3e629e7cfba91e188ffe704a0
johan borg <56513637+johanborg1996@users.noreply.github.com>
2020-05-07 11:45:34 +0200

    first plot

--- online weather.py
+++ online weather.py
@@ -1,6 +1,9 @@
 import pandas as pd 
 import numpy as np 
 import csv
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.cbook as cbook
 
 def readfiles():
 
@@ -30,7 +33,40 @@ def readfiles():
 
 def temperature(list_temperatur):
 
+years = mdates.YearLocator()   # every year
+months = mdates.MonthLocator()  # every month
+years_fmt = mdates.DateFormatter('%Y')
 
+# Load a numpy structured array from yahoo csv data with fields date, open,
+# close, volume, adj_close from the mpl-data/example directory.  This array
+# stores the date as an np.datetime64 with a day unit ('D') in the 'date'
+# column.
+with cbook.get_sample_data('goog.npz') as datafile:
+    data = np.load(datafile)['price_data']
+
+fig, ax = plt.subplots()
+ax.plot('date', 'adj_close', data=data)
+
+# format the ticks
+ax.xaxis.set_major_locator(years)
+ax.xaxis.set_major_formatter(years_fmt)
+ax.xaxis.set_minor_locator(months)
+
+# round to nearest years.
+datemin = np.datetime64(data['date'][0], 'Y')
+datemax = np.datetime64(data['date'][-1], 'Y') + np.timedelta64(1, 'Y')
+ax.set_xlim(datemin, datemax)
+
+# format the coords message box
+ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
+ax.format_ydata = lambda x: '$%1.2f' % x  # format the price.
+ax.grid(True)
+
+# rotates and right aligns the x labels, and moves the bottom of the
+# axes up to make room for them
+fig.autofmt_xdate()
+
+plt.show()
 
 def sunshine(list_sol):
     "blabl"
@@ -55,11 +91,9 @@ def main():
         if answer == "1":
             temperature(list_temperatur)
         if answer == "2":
-            temperature(list_temperatur)
+            sunshine(list_sol)
         if answer == "3":
-            temperature(list_temperatur)
+            rain(list_neder)
 
 main()
 
-
-print("hello")
